9e0977ad19af2179761cba489e4b9bc2ef344ace
Tamnm <ngominhtam26112005@gmail.com>
2026-03-05 23:04:53 +0700

    Tamnm: init router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 function App() {
-
   return (
-    <>
-        <div className="bg-primary w-4 h-4"> hello</div>
-    </>
-  )
+    <div className="bg-background min-h-screen">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
